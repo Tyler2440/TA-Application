849ac888dc6812b62527252fb77ef92fe94b5bd6
Tyler2440 <u1211154@eng.utah.edu>
2021-09-21 16:38:50 -0600

    Added unworking Resume Upload functionality to HomeController and Details. Added configuration.json file to hold where resume file uploads save to.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,8 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using TAApplication.Models;
@@ -12,6 +16,7 @@ namespace TAApplication.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        
 
         public HomeController(ILogger<HomeController> logger)
         {
@@ -47,6 +52,42 @@ namespace TAApplication.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> FileUpload(List<IFormFile> files)
+        {
+            try
+            {
+                if (files.Count != 1) 
+                { 
+                    return BadRequest(new { message = "Must Submit One File" }); 
+                }
+                var _config = JObject.Parse(JsonConvert.SerializeObject("configuration.json"));
+                long size = files.Sum(f => f.Length); 
+                var new_name = ""; 
+                var extension = "";
+                var filePaths = new List<string>();
+                foreach (var formFile in files)
+                {
+                    // check formFile length to make sure file is a reasonable size
+                    if (formFile.Length > 10000000 || formFile.Length == 0)
+                        return BadRequest(new { message = "File is too large or is empty. Please upload a file smaller than 10MB." });
+              
+                    new_name = formFile.FileName;  // warning: really should use a new name!
+                    var filePath = Path.Combine(_config["StoreFilePath"].ToString(), Path.GetRandomFileName());
+                    // save the file
+                    using (var stream = new FileStream(filePath, FileMode.Create))
+                    {
+                        await formFile.CopyToAsync(stream);
+                    }
+                }
+                return Ok(new { message = new_name, count = files.Count, size, filePaths });
+            }
+            catch (Exception)
+            {
+                return BadRequest(new { message = "Something went wrong. Try again later." });
+            }           
         }
     }
 }
